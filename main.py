0341cfce2233f3dd2bceed420099fd249f1e4875
VadimProg1 <b.vadim.alexeyevich@gmail.com>
2026-02-22 15:27:43 +0700

    Added loop for agent

--- main.py
+++ main.py
@@ -28,24 +28,21 @@ def print_metadata(user_input, response):
     print(f"Response tokens: {candidates_tokens_count}")
     
 
-def main():
-    model_name = "gemini-2.5-flash"
+def call_llm(messages):
+    v3model_name = "gemini-3-flash-preview"
+    v2model_name = "gemini-2.5-flash"
     client = get_llm_client()
-    user_input = setup_parser().parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=user_input.prompt)])]
-    
     response = client.models.generate_content(
-        model=model_name,
+        model=v2model_name,
         contents=messages,
         config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
     )
     if response.usage_metadata == None:
         raise RuntimeError("Usage metadata from response not found!")
-    
-    if user_input.verbose:
-        print_metadata(user_input, response)
+    return response
 
-    if response.function_calls != None or len(response.function_calls) > 0:
+def use_functions(response, user_input):
+    if response.function_calls != None and len(response.function_calls) > 0:
         function_results = []
         for function_call in response.function_calls:
             function_call_result = call_function(function_call, user_input.verbose)
@@ -54,12 +51,33 @@ def main():
             result_parts = function_call_result.parts
             if result_parts[0].function_response == None or result_parts[0].function_response.response == None:
                 raise Exception("Function response is empty")
-            function_results = result_parts[0]
+            function_results = result_parts
             if user_input.verbose:
                 print(f"-> {result_parts[0].function_response.response}")
+        return function_results
     else:
-        print(f"Response:\n{response.text}")
+        print(f"Final response:\n{response.text}")
+
+def main():
+    user_input = setup_parser().parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=user_input.prompt)])]
+    had_final_response = False
+    for _ in range(10):
+        response = call_llm(messages)
     
+        if user_input.verbose:
+            print_metadata(user_input, response)
+
+        for candidate in response.candidates:
+            messages.append(candidate)
+
+        functions_call_result = use_functions(response, user_input)
+        if functions_call_result == None:
+            had_final_response = True
+            break
+        messages.append(types.Content(role="user", parts=functions_call_result))
+    if not had_final_response:
+        exit(1)
 
 if __name__ == "__main__":
     main()
